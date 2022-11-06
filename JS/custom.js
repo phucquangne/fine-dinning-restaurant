f83e5a4b9ec396853c5b5b88d762847bb7834489
phucquangne <phucquang2001@gmail.com>
2022-11-06 12:58:07 +0700

    Upload

---
+++ JS/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('#ban').owlCarousel({
+    loop:true,
+   
+    nav:true,
+    items:1,
+ })  
+})// JavaScript Document
+
+
+
+
+
+$(document).ready(function(){
+	$('#combo').owlCarousel({
+    loop:true,
+    margin:60,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+})// JavaScript Document
